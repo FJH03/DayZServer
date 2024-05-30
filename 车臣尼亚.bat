fba06ba491cb265bc70f0d5c87f4334eb3df0812
FJH_03 <fengjiahe5@gmail.com>
2024-05-30 18:33:32 +0800

    更新车臣尼亚服务器模组，删除系统自动生成的文件

--- 车臣尼亚.bat
+++ 车臣尼亚.bat
@@ -12,7 +12,7 @@ title %wat% Watchdog
 
 echo (%time%) %wat% started.
 
-start "Datz_SA" /wait /affinity FF /high "DayZServer_x64.exe" -config=chernarusplus.cfg -profiles=O:\SteamLibrary\steamapps\common\DayZServer\Profiles\survive
+start "Datz_SA" /wait /affinity FF /high "DayZServer_x64.exe" -config=chernarusplus.cfg -mod=@Better-Snap-Sounds;@CF;@Dabs_Framework;@DayZ-Expansion-AI;@DayZ-Expansion-Book;@DayZ-Expansion-Core;@DayZ-Expansion-Groups;@DayZ-Expansion-Navigation;@ViewInventoryAnimation -profiles=O:\SteamLibrary\steamapps\common\DayZServer\Profiles\survive
 
 echo (%time%) %wat% closed or crashed, restarting.
 
