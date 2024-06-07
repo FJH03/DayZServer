c8cb1c285a2a64116b5087835ea5fc11b0eda6ca
FJH_03 <fengjiahe5@gmail.com>
2024-06-07 18:38:17 +0800

    更新了冬天车臣尼亚与冬天鹿岛DM模式启动文件

---
+++ 冬天车臣尼亚.bat
@@ -0,0 +1,19 @@
+@echo off
+
+cls
+
+set version=1.0
+
+set wat=Dayz SA
+
+title %wat% Watchdog
+
+:watchdog
+
+echo (%time%) %wat% started.
+
+start "Datz_SA" /wait /affinity FF /high "DayZServer_x64.exe" -config=chernarusplus_snow.cfg -servermod=@DayZ-Dynamic-AI-Addon -mod=@Better-Snap-Sounds;@CF;@Dabs_Framework;@DayZ-Expansion-AI;@DayZ-Expansion-Book;@DayZ-Expansion-Core;@DayZ-Expansion-Groups;@DayZ-Expansion-Navigation;@Drones;@GoreZ;@InediaInfectedAI;@ViewInventoryAnimation;@VPP;@Winter_Chernarus_V2;@ZenSleep -profiles=.\Profiles\survive
+
+echo (%time%) %wat% closed or crashed, restarting.
+
+goto watchdog
